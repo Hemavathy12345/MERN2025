17f9e8dc28c6f868d1d0d44a1d096c3f69826669
Hemavathy J <jhemavathyj@gmail.com>
2025-01-21 10:10:45 +0530

    modify about

--- secefrontend/src/components/FunctionalComponents/About.jsx
+++ secefrontend/src/components/FunctionalComponents/About.jsx
@@ -1,7 +1,16 @@
+import {useState} from "react"
 const About = ()=>{
+    var [text,setText]=useState("Hema")
+    function handleText(e){
+        setText(e.target.value)
+    }
     return (
         <div>
             <h2>THIS IS A FUNCTIONAL COMPONENRT</h2>
+            <h2>TextArea:</h2>
+            {/* <textarea value={text} onChange={(event)=>setText(event.target.value)}/> */}
+            <textarea value={text} onChange={handleText}/>
+            <p>The text is {text}</p>
         </div>
     )
 }
